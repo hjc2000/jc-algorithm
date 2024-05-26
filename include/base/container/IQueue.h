d8df0dc0e74942140601a4587d40309aa92c8266
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-26 21:57:09 +0800

    container

---
+++ include/base/container/IQueue.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<stdint.h>
+
+namespace base
+{
+	/// <summary>
+	///		队列接口
+	/// </summary>
+	template<typename T>
+	class IQueue
+	{
+	public:
+		virtual ~IQueue() { }
+
+	public:
+		virtual uint64_t Count() const = 0;
+		virtual T Dequeue() = 0;
+		virtual bool TryDequeue(T &out) = 0;
+		virtual void Enqueue(T obj) = 0;
+		virtual void Clear() = 0;
+	};
+}
