2daf01dabfc4dcdf1890e15d1cd423c2dcb3adf8
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-31 23:57:40 +0800

    void TestRentedPtrFactory()

--- include/base/RentedPtrFactory.h
+++ include/base/RentedPtrFactory.h
@@ -23,11 +23,4 @@ namespace base
 			return ret;
 		}
 	};
-
-	/// <summary>
-	///		测试函数。测试函数的存在是为了在本库中就将泛型的 RentedPtrFactory::Create 方法
-	///		给实例化，这样编译器和 inlinesenser 才能在有错的情况下检测出错误，否则 C++ 模板
-	///		很难在没有被实例化的情况下检测错误。
-	/// </summary>
-	void TestRentedPtrFactory();
 }
