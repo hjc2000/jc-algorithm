e56cf6277487a06e29795b26ab639ef569eb1aec
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-23 14:54:10 +0800

    class RentedPtrFactory

--- include/base/HandleWrapper.h
+++ include/base/HandleWrapper.h
@@ -2,15 +2,14 @@
 
 namespace base
 {
-	/// <summary>
-	///		句柄包装对象。
-	///		* 与在 ffmpeg-wrapper 中大量使用的 Wrapper 类不同，Wrapper 类是包装
-	///		  位于堆上的对象的，Wrapper<T> 类中持有的是 T* 字段，而不是 T 字段。而 HandleWrapper
-	///		  HandleWrapper<T> 持有的是 T 字段而不是 T* 字段。即句柄包装类直接将句柄本身作为
-	///		  自己的字段，而不是在堆上构造一个句柄对象然后将指向它的指针作为字段。
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	template<typename T>
+	/// @brief 句柄包装对象。
+	/// @note 与在 ffmpeg-wrapper 中大量使用的 Wrapper 类不同，Wrapper 类是包装
+	/// 	  位于堆上的对象的，Wrapper<T> 类中持有的是 T* 字段，而不是 T 字段。而 HandleWrapper
+	/// 	  HandleWrapper<T> 持有的是 T 字段而不是 T* 字段。即句柄包装类直接将句柄本身作为
+	/// 	  自己的字段，而不是在堆上构造一个句柄对象然后将指向它的指针作为字段。
+	///
+	/// @tparam T
+	template <typename T>
 	class HandleWrapper
 	{
 	public:
--- include/base/RentedPtrFactory.h
+++ include/base/RentedPtrFactory.h
@@ -1,25 +1,24 @@
 #pragma once
-#include<memory>
+#include <memory>
 
 namespace base
 {
+	/// @brief 借用指针工厂
 	class RentedPtrFactory
 	{
 	public:
-		/// <summary>
-		///		利用传进来的裸指针构造一个共享指针。这个共享指针的删除器什么也不做，
-		///		不会释放内存。
-		/// 
-		///		这么做是为了让一些例如静态变量或手动管理生命周期的变量能够被 shared_ptr
-		///		套壳，从而兼容那些需要 shared_ptr 的 API。
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="o"></param>
-		/// <returns></returns>
-		template<typename T>
+		/// @brief 创建一个借用指针。
+		/// @note 借用指针实际上是一格共享指针，这个共享指针的删除器什么也不做，不会释放内存。
+		/// @note 这么做是为了让一些例如静态变量或手动管理生命周期的变量能够被 shared_ptr
+		/// 	  套壳，从而兼容那些需要 shared_ptr 的 API。
+		///
+		/// @tparam T
+		/// @param o
+		/// @return
+		template <typename T>
 		static std::shared_ptr<T> Create(T *o)
 		{
-			std::shared_ptr<T> ret { o, [](T *o) { } };
+			std::shared_ptr<T> ret{o, [](T *o) {}};
 			return ret;
 		}
 	};
