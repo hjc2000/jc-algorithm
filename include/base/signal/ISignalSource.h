77cfb1ae18df6d87ca8f981c605a5c742508dd5c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-29 17:27:06 +0800

    /// @note 这种信号源通常是一个关于时间的函数，能够根据任意的时间值计算出采样值。

--- include/base/signal/ISignalSource.h
+++ include/base/signal/ISignalSource.h
@@ -3,6 +3,8 @@
 namespace base
 {
 	/// @brief 能够对任意时刻进行采样的信号源接口。
+	/// @note 这种信号源通常是一个关于时间的函数，能够根据任意的时间值计算出采样值。
+	///
 	/// @tparam SignalType
 	/// @tparam TimeType
 	template <typename SignalType, typename TimeType>
