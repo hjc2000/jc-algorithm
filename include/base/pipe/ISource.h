2a948c889e7e88ab51c53b94ca514057fb9e288c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-01 23:36:22 +0800

    virtual int PumpTo(base::IConsumer<T> &consumer, 						   base::CancellationToken &cancellationToken)

--- include/base/pipe/ISource.h
+++ include/base/pipe/ISource.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <base/pipe/IPipeSource.h>
+#include <base/task/CancellationToken.h>
+#include <base/task/TaskCanceledException.h>
 
 namespace base
 {
@@ -15,5 +18,31 @@ namespace base
 		/// @param data 这里传入的是引用。派生类可以修改 data。
 		/// @return 成功返回 0，失败返回负数的错误代码。
 		virtual int ReadData(T &data) = 0;
+
+		/// @brief 将数据泵送给消费者
+		/// @param consumer
+		/// @param cancellationToken
+		/// @return
+		virtual int PumpTo(base::IConsumer<T> &consumer,
+						   base::CancellationToken &cancellationToken)
+		{
+			while (true)
+			{
+				if (cancellationToken.IsCancellationRequested())
+				{
+					throw base::TaskCanceledException{"PumpTo 函数被取消。"};
+				}
+
+				T data{};
+				int ret = ReadData(data);
+				if (ret < 0)
+				{
+					consumer.Flush();
+					return ret;
+				}
+
+				consumer.SendData(data);
+			}
+		}
 	};
 }
