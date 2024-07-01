2e3538adf68a30b9991c6ce32a33cbe429351260
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-02 00:12:56 +0800

    注释

--- include/base/pipe/ISource.h
+++ include/base/pipe/ISource.h
@@ -20,9 +20,17 @@ namespace base
 		virtual int ReadData(T &data) = 0;
 
 		/// @brief 将数据泵送给消费者
-		/// @param consumer
-		/// @param cancellationToken
-		/// @return
+		///
+		/// @param consumer 消费者
+		///
+		/// @param cancellationToken 取消令牌
+		/// @note 主动取消不会冲洗消费者。
+		///
+		/// @return 成功返回 0，失败返回负数的错误代码
+		/// @note 此错误代码直接来自 ReadData。
+		/// @note 调用本对象的 ReadData 方法读取数据，将数据送给消费者的过程中，如果遇到 ReadData
+		/// 方法返回了负数，说明 数据源——本对象 发生了错误，可能是读取到文件尾，没有更多数据了，
+		/// 或者其他错误。这时候会冲洗消费者，然后原封不动地返回 ReadData 方法所返回的错误代码。
 		virtual int PumpTo(base::IConsumer<T> &consumer,
 						   std::shared_ptr<base::CancellationToken> cancellationToken)
 		{
